eb7998c25aeddaffc026157bf8e04662f5a093d9
djk <dkats12@gmail.com>
2017-07-03 20:08:58 -0400

    Fixed CV link

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 document.write('<header>');
 document.write('<ul class="menu">');
 document.write('	<li class="noclick">Dan Kats</li>');
-document.write('	<li onclick="window.open(\'files/cv.pdf\')" id="header_cv">CV</li>');
+document.write('	<li onclick="window.open(\'http://www.dankats.com/files/CV.pdf\')" id="header_cv">CV</li>');
 document.write('	<li onclick="window.open(\'education.html\')" id="header_edu">Education</li>');
 document.write('	<li onclick="window.open(\'projects.html\')" id="header_proj">Projects</li>');
 document.write('	<li onclick="window.open(\'research.html\')" id="header_res">Research</li>');
