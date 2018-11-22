7628108ab559945afb972f3fd60a6f7b61f7d03f
Dan Kats <djk175@Dans-MBP.home>
2018-11-22 14:55:30 -0500

    Removed education from header

--- js/header.js
+++ js/header.js
@@ -9,7 +9,7 @@ function header(level = 0) {
 	document.write('<ul class="menu">');
 	document.write('	<li class="noclick">Dan Kats</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'files/CV.pdf\')" id="header_cv">CV</li>');
-	document.write('	<li onclick="window.open(\'' + dir + 'education.html\',\'_self\')" id="header_edu">Education</li>');
+	// document.write('	<li onclick="window.open(\'' + dir + 'education.html\',\'_self\')" id="header_edu">Education</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'projects.html\',\'_self\')" id="header_proj">Projects</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'research.html\',\'_self\')" id="header_res">Research</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'extracurriculars.html\',\'_self\')" id="header_extracurr">Extracurriculars</li>');
