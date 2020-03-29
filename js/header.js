8f6983d7d95d6c2f10d31e02e8b99df5afa062bc
Dan Kats <djk175@Dans-MBP.fios-router.home>
2020-03-29 14:43:46 -0400

    Merged over from Residency Campaign

--- js/header.js
+++ js/header.js
@@ -8,12 +8,13 @@ function header(level = 0) {
 	document.write('<header>');
 	document.write('<ul class="menu">');
 	document.write('	<li class="noclick">Dan Kats</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'who.html\',\'_self\')" id="header_who">Who is Dan?</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'files/CV.pdf\')" id="header_cv">CV</li>');
-	// document.write('	<li onclick="window.open(\'' + dir + 'education.html\',\'_self\')" id="header_edu">Education</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'fourth-year.html\',\'_self\')" id="header_fourth">Fourth Year Plans</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'projects.html\',\'_self\')" id="header_proj">Projects</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'research.html\',\'_self\')" id="header_res">Research</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'hobbies.html\',\'_self\')" id="header_extracurr">Hobbies</li>');
-	document.write('	<li onclick="window.open(\'' + dir + 'thoughts.html\',\'_self\')" id="header_thoughts">My Thoughts</li>');
+	document.write('	<li onclick="window.open(\'' + dir + 'thoughts.html\',\'_self\')" id="header_thoughts">Dan\'s Thoughts</li>');
 	document.write('	<li onclick="window.open(\'' + dir + 'quotations.html\',\'_self\')" id="header_quotations">Quotations</li>');
 	document.write('</ul>');
 	document.write('</header>');
